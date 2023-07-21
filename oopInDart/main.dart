2a88ac89dc7154a6a484bf76f3687f387ca610e1
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-07-21 10:03:40 -0700

    .

---
+++ oopInDart/main.dart
@@ -0,0 +1,12 @@
+class Person {
+  //properties or fields
+  String? name;
+  String? complection;
+  String? profession;
+  int? age;
+  //methods or functions
+  void display() {
+    print(
+        "My  name is $name.\nI am $age years old.\nMy complection is $complection.\nBy profession i am a $profession.");
+  }
+}
