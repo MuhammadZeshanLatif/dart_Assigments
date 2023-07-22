ae863dc674526c431ae752046c63f42e8e2ee19c
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-07-22 03:16:03 -0700

    s

---
+++ oopInDart/constructor.dart
@@ -0,0 +1,16 @@
+class Student{
+  //Thes are the properties or fields of Student object
+  String? name;
+  String? adress;
+  double? age;
+  int?roll;
+  //here we are making a cunstor with same name as class with out any return type like voide public etc.
+  Student(String myName,String myAdress,double myAge,int myRoll){
+      this.name=myName;
+      this.adress=myAdress;
+      this.age=myAge;
+      this
+  }
+
+
+}
--- oopInDart/main.dart
+++ oopInDart/main.dart
@@ -1,4 +1,26 @@
-import 'showData.dart';
+class Person {
+  //properties or fields
+  String? name;
+  String? complection;
+  String? profession;
+  int? age;
+  //methods or functions
+  void display() {
+    print(
+        "My  name is $name.\nI am $age years old.\nMy complection is $complection.\nBy profession i am a $profession.");
+  }
+}
+ class BiCycle{
+  String? color;
+  int? size;
+  int? currentSpeed;
+  void display(int speed){
+    currentSpeed=speed;
+  }
+  void show(){
+    print("Color of my biCycle is $color.\n It is $size cm in size.\n its speed is $currentSpeed");
+  }
+ }
  void main(){
   Person prsn=Person();
   BiCycle cycle=BiCycle();
